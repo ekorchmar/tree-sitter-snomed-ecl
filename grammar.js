8123869007419cbbdc67205b84952d2f15f995ee
Eduard Korchmar <korchmar.eduard@gmail.com>
2024-04-30 20:33:03 +0200

    Add rules up to constraint operators

--- grammar.js
+++ grammar.js
@@ -2,8 +2,8 @@ module.exports = grammar({
   name: 'snomed-ecl',
 
   rules: {
-    // The Expression Constraint:
-    expression_constraint: $ => seq(
+    // The Entry point: expression Constraint:
+    expressionConstraint: $ => seq(
       $._ws,
       choice(
         $.refinedExpressionConstraint,
@@ -59,12 +59,12 @@ module.exports = grammar({
       $.subExpressionConstraint,
       repeat1(seq(
         $._ws,
-        $.dot,
+        $._dot,
         $._dottedExpressiontAttribute,
       ))
     ),
 
-    _dottedExpressiontAttribute: $ => seq($.dot, $._ws, $.eclAttributeName),
+    _dottedExpressiontAttribute: $ => seq($._dot, $._ws, $.eclAttributeName),
 
   /* ABNF excerpt:
 
@@ -123,12 +123,119 @@ subExpressionConstraint =
 
     _eclFocusConcept: $ => choice(
       $.eclConceptReference,
-      $.wildCard,
+      $._wildCard,
       $.altIdentifier,
     ),
 
-    dot: _ => '.',
+    memberOf: _ => seq(
+      "^",
+      optional(seq(
+        $._ws,
+        "[",
+        $._ws,
+        choice(
+          $.refsetFieldNameSet,
+          $._wildCard,
+        ),
+        $._ws,
+        "]",
+      )),
+    ),
 
+    // Field names
+    refsetFieldNameSet: $ => seq(
+      refsetfieldName,
+      repeat(seq(
+        $._ws,
+        ",",
+        $.refsetfieldName,
+      ))
+    ),
+
+    refsetfieldName: _ => repeat1($._alpha),
+
+    // Concept reference (SCTID)
+    _conceptNameInId: $ => seq(
+        $._ws,
+        "|",
+        $._ws,
+        $.term,
+        $._ws,
+        "|",
+      ),
+
+    eclConceptReference: $ => seq(
+      $._conceptId,
+      optional($._conceptNameInId),
+    ),
+    eclConceptReferenceSet: $ => seq(
+      "(",
+      $._ws,
+      $.eclConceptReference,
+      repeat1(seq(
+        $._mws,
+        ",",
+        $._ws,
+        $.eclConceptReference,
+      )),
+      $._ws,
+      ")",
+    ),
+    _conceptId: _ => $.sctId,
+    term: $ => seq(
+      repeat1($._nonwsNonPipe),
+      repeat(seq(
+        repeat1($._SP),
+        repeat1($._nonwsNonPipe),
+      ))
+    ),
+
+    // Alternative identifier
+    altIdentifier: $ => seq(
+      choice(
+        // With or without quotes
+        seq(
+          $._QM,
+          $.altIdentifierSchemeAlias,
+          "#",
+          $.altIdentifierCodeWithinQuotes,
+          $._QM,
+        ),
+        seq(
+          $.altIdentifierSchemeAlias,
+          "#",
+          $.altIdentifierCodeWithoutQuotes,
+        ),
+      ),
+      optional($._conceptNameInId),
+    ),
+
+    altIdentifierSchemeAlias: $ => seq(
+      $._alpha,
+      repeat(choice(
+        $._alpha,
+        $._dash,
+        $._integerValue,
+      )
+    )),
+
+    altIdentifierCodeWithinQuotes: $ => repeat1($._anyNonEscapedChar),
+    altIdentifierCodeWithoutQuotes: $ => repeat1(choice(
+      $._alpha,
+      $._digit,
+      $._dash,
+      $._dot,
+      "_",
+    )),
+
+    // Literals
+    _dot: _ => '.',
+    _wildCard: _ => '*',
+    _SP: _ => ' ',
+
+    // Tokens
+    _alpha: _ => /[a-zA-Z]/,
+    
 
   }
 });
